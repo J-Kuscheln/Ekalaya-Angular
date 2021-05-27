fc8df24a8256272df7357a65c370ecb29ea67da4
J-Kuscheln <mandalasatria.jaya@gmail.com>
2021-05-27 22:06:11 +0800

    Homepage
    Aing punya kerjaan

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,6 @@
-import { Subscription } from 'rxjs';
+import { interval, Subscription } from 'rxjs';
 import { SessionService } from './../services/session.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, SystemJsNgModuleLoader } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
@@ -13,12 +13,39 @@ export class HomePageComponent implements OnInit {
   private subscription:Subscription[] = new Array<Subscription>();
   constructor(private session:SessionService) { }
 
+  public counter:number;
+  intervalID: number;
+
   ngOnInit(): void {
     this.subscription[0] = this.session.myUserFirstName$.subscribe(resp => this.userFirstName=resp);
     this.subscription[1] = this.session.myUserLastName$.subscribe(resp=>this.userLastName=resp);
     this.subscription[2] = this.session.myStatus$.subscribe(/*do something*/);
-
     this.session.checkSession();
+
+    this.counter = 1;
+    
+    const intervals$ = interval(10000);
+    this.subscription[3] = intervals$.subscribe(val => this.next())
+  }
+
+  currentSlide(i: number){
+    this.counter = i;
+  }
+
+  next(){
+    if (this.counter == 3){
+      this.counter = 1;
+    } else {
+      this.counter ++;
+    }
+  }
+
+  prev(){
+    if(this.counter == 1){
+      this.counter = 3;
+    } else {
+      this.counter --;
+    }
   }
 
 }
